010a94c955dc28a7e49a20ed924900af1180ce48
Ojas S <ojas1807@gmail.com>
2025-06-27 18:02:44 -0400

    Init commit

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require("/home/f1sh/Documents/doc/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
+  require("/home/f1sh/Documents/doc/node_modules/infima/dist/css/default/default.css"),
+  require("/home/f1sh/Documents/doc/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/home/f1sh/Documents/doc/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/home/f1sh/Documents/doc/src/css/custom.css"),
+];
